60755126d43319fda3007c1c6af984156de5f148
Aurora Villa <villaaurora375@gmail.com>
2025-04-01 21:01:18 -0600

    Hash User

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 use Illuminate\View\View;
-use Illuminate\Support\Facades\Hash;
+
 
 class GuestLayout extends Component
 {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create[(
+            'name'=> 'Aurora',
+            'email' => 'villaaurora375@gmail.com'
+            'password' => Hash::make ('12345678'), 
+            'email_verified_at' => now(),
+        )]
     }
 }
